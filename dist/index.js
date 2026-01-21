efbafb20e7f258714894ee39c7c4d3c1c1f5e2aa
Daniil <efremenkodana@gmail.com>
2026-01-21 14:33:02 +0300

    Implemented basic browser management

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,19 @@
 import { Builder, Browser } from 'selenium-webdriver';
 import { Options } from 'selenium-webdriver/chrome.js';
-
 async function main() {
     const options = new Options();
     options.addArguments('--no-sandbox');
-
     const driver = await new Builder()
         .forBrowser(Browser.CHROME)
         .setChromeOptions(options)
         .build();
-
     try {
         await driver.get('https://www.selenium.dev/selenium/web/web-form.html');
         console.log('Page loaded');
         await driver.sleep(5000);
-    } finally {
+    }
+    finally {
         await driver.quit();
     }
 }
-
 main();
